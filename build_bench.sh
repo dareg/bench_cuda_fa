bb98a60b1c0d410906c2953f5001f32c19596dd4
Judicaël Grasset <judicael.grasset@meteo.fr>
2023-11-22 11:07:56 +0000

    improve test

--- build_bench.sh
+++ build_bench.sh
@@ -2,28 +2,33 @@
 
 ROOT=$PWD
 
+module load nvhpc/23.9
+
 INC_FIAT=fiat/install_nvidia/module/fiat/
 INC_PAR=fiat/install_nvidia/module/parkind_dp/
 LD_FIAT="fiat/install_nvidia/lib64/ -lfiat" 
 
+NVF="nvfortran -O2 -acc=gpu -I$INC_PAR -I$INC_FIAT -L$LD_FIAT"
 
-#v0.2.2
-echo "Building v0.2.2"
-LD_FA=install_field_api/lib64/
-INC_FA=install_field_api/include/field_api_dp/ 
-nvfortran -O2 -acc=gpu main.F90 -I$INC_PAR -I$INC_FIAT -I$INC_FA -L$LD_FIAT -L$LD_FA -lfield_api_dp
-mv a.out bench_v0.2.2
+for name in contiguous not-contiguous; do
+	#v0.2.2
+	echo "Building $name v0.2.2"
+	LD_FA=install_field_api/lib64/
+	INC_FA=install_field_api/include/field_api_dp/ 
+	$NVF $name.F90 -I$INC_FA  -L$LD_FA -lfield_api_dp
+	mv a.out "bench_${name}_v0.2.2"
 
-#pinning
-echo "Building pinning"
-LD_FA=install_pinning/lib64/
-INC_FA=install_pinning/include/field_api_dp/ 
-nvfortran -O2 -acc=gpu -cuda main.F90 -I$INC_PAR -I$INC_FIAT -I$INC_FA -L$LD_FIAT -L$LD_FA -lfield_api_dp
-mv a.out bench_pinning
+	#pinning
+	echo "Building $name pinning"
+	LD_FA=install_pinning/lib64/
+	INC_FA=install_pinning/include/field_api_dp/ 
+	$NVF -cuda $name.F90 -I$INC_FA  -L$LD_FA -lfield_api_dp
+	mv a.out "bench_${name}_pinning"
 
-#lukas
-echo "Building lukas"
-LD_FA=install_lukas/lib64/
-INC_FA=install_lukas/include/field_api_dp/ 
-nvfortran -O2 -acc=gpu -cuda main.F90 -I$INC_PAR -I$INC_FIAT -I$INC_FA -L$LD_FIAT -L$LD_FA -lfield_api_dp
-mv a.out bench_lukas
+	#lukas
+	echo "Building $name lukas"
+	LD_FA=install_lukas/lib64/
+	INC_FA=install_lukas/include/field_api_dp/ 
+	$NVF -cuda $name.F90 -I$INC_FA  -L$LD_FA -lfield_api_dp
+	mv a.out "bench_${name}_lukas"
+done
--- run_bench.sh
+++ run_bench.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
+module load nvhpc/23.9
+
 case $HOSTNAME in 
 	(belenosndl* | taranisndl*) ;;
 	(*) echo "Not on ndl node" && exit 1 ;;
 esac
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/fiat/install_nvidia/lib64
-NVPROF=/opt/softs/nvidia/hpc_sdk/Linux_x86_64/23.9/cuda/12.2/bin/nvprof
-NVPROF=nvprof
+NVPROF="nvprof --normalized-time-unit ms --csv"
 
-$NVPROF ./bench_v0.2.2
-$NVPROF ./bench_pinning
-$NVPROF ./bench_lukas
+echo "#all time in ms" > res.csv
+echo "v0.2.2, pinning, lukas" >> res.csv
+
+for name in contiguous not-contiguous; do
+	for version in v0.2.2 pinning lukas; do 
+		echo "Bench $name $version"
+		$NVPROF ./bench_${name}_${version} &> log_${name}_${version}.csv
+	done
+	for dir in HtoD DtoH; do
+		echo -n "$name $dir," >> res.csv
+		for version in v0.2.2 pinning lukas; do 
+			awk -F "," "/CUDA memcpy $dir/ {printf \"%f,\",\$3}" log_${name}_${version}.csv >> res.csv
+		done
+		echo "" >> res.csv
+	done
+	echo "" >> res.csv
+done
+
+cat res.csv
